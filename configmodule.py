b4ace28bf312f57e4d014d053f13b9aec75d3847
DansAtk <dansatk@outlook.com>
2021-01-20 17:18:38 -0500

    Added saving/loading config from a file. Added user time module.

--- configmodule.py
+++ configmodule.py
@@ -1,18 +1,52 @@
+import sys
+import json
+
 import commandsmodule
 import config
 
 includes = {}
 
 change = commandsmodule.command('change', __name__)
+push = commandsmodule.command('push', __name__)
+pull = commandsmodule.command('pull', __name__)
 
 def init():
-    includes.update({change.name : change})
+    print("Startup config.\n")
+    pullF()
+    includes.update({push.name : push, pull.name : pull, change.name : change})
+    push.description = "Pushes the current config to a file to make it persistent."
+    push.function = 'pushF'
+    pull.description = "Pulls the saved config from file."
+    pull.function = 'pullF'
     change.description = "Used to alter bot system variables. Usable by admins only."
     change.function = 'changeF'
     change.parameters.update({'trigger' : commandsmodule.command('trigger', __name__)})
     change.parameters['trigger'].description = "Set the command trigger to a different set of characters."
     change.parameters['trigger'].function = 'triggerF'
-    config.imported.append('configmodule')
+    config.imports.append('configmodule')
+
+def cleanUp():
+    print("Saving config...")
+    pushF()
+    print("done!")
+
+def pushF(message = ""):
+    print("Pushing config to file.")
+    try:
+        with open("./conf.json", 'w') as conf:
+            json.dump(config.settings, conf)
+        print("Success!")
+    except:
+        print("Failure!")
+
+def pullF(message = ""):
+    print("Pulling config from file...")
+    try:
+        with open("./conf.json", 'r') as conf:
+            config.settings = json.load(conf)
+        print("Success!")
+    except FileNotFoundError:
+        print("No config file found!")
 
 def changeF(message):
     print("Please specify a parameter. Available parameters: " + ', '.join(change.parameters) + ", help.")
@@ -20,7 +54,7 @@ def changeF(message):
 def triggerF(message):
     if len(message) == 1:
         if len(message[0]) < 3:
-            config.trigger = message[0]
+            config.settings['trigger'] = message[0]
         else:
             print("Try a shorter trigger.")
     else:
