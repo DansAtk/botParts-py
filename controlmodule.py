9018cf10f58560b89767773c50f52f1f629f7eff
DansAtk <dansatk@outlook.com>
2021-01-22 21:21:18 -0500

    Added alias 'quit' for 'exit' command.

--- controlmodule.py
+++ controlmodule.py
@@ -6,11 +6,15 @@ import commandsmodule
 includes = {}
 
 exit = commandsmodule.command('exit', __name__)
+quit = commandsmodule.command('quit', __name__)
 
 def init():
     includes.update({exit.name : exit})
     exit.description = "Closes the bot gracefully."
     exit.function = 'exitF'
+    includes.update({quit.name : quit})
+    quit.description = "Closes the bot gracefully."
+    quit.function = 'exitF'
     config.imports.append('controlmodule')
 
 def exitF(message):
