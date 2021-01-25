6ce96e2e8e62ccf4118603b749a5a2604717c1a7
dansatk <dansatk@outlook.com>
2021-01-24 22:13:55 -0500

    Moved to WSL2

---
+++ messparse.py
@@ -0,0 +1,28 @@
+# parses the incoming message
+
+import config
+import sys
+
+def read(text):
+    if text.startswith(config.settings['trigger']):
+        message = text[1:].split(" ")
+        valid = 0
+        for module in config.imports:
+            if message[0] in sys.modules[module].includes.keys():
+                valid = 1
+                i = 1 
+                pack = sys.modules[module].includes[message[0]]
+                for word in message[i:]:
+                    if word in pack.parameters.keys():
+                        pack = pack.parameters[word]
+                        i += 1
+                if ' '.join(message[i:]) == "help":
+                    print(pack.help())
+                else:
+                    pack.execute(message[i:])
+
+        if valid == 0:
+            print("Invalid command!")
+
+if __name__ == "__main__":
+    print("No main.")
