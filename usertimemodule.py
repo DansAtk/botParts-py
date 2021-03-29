c28c7c8d3460a801c6bd8d577ef0969f7f3e7f2b
dansatk <dansatk@outlook.com>
2021-03-29 18:32:18 -0400

    Added functions for creating new databases, getting the current active database, setting the current active database and listing all databases known to the bot. Allows any additional modules to add required information/structures to the database on init by including a dbinit function. Converted to new path handling using pathlib.

--- usertimemodule.py
+++ usertimemodule.py
@@ -30,10 +30,11 @@ def setF(message):
         print(time.parameters['set'].paramError(message))
     else:
         print("The time has been set.")
+
 def getTime():
     return datetime.datetime.now().strftime("%H:%M:%S")
 
 if __name__ == "__main__":
-    print("No main.")
+    print("Adds functionality for managing users located in different timezones. Should be merged into usermodule. No main.")
 else:
     init()
