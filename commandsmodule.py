b28ac1b80dfc3330ef9cc0e11d29542c8691f5b9
dansatk <dansatk@outlook.com>
2021-03-29 13:51:16 -0400

    Merged updated commandmodule and run from database_use branch, removed messparse.

--- commandsmodule.py
+++ commandsmodule.py
@@ -6,9 +6,6 @@ import config
 
 includes = {}
 
-def init():
-    config.imports.append('commandsmodule')
-
 class command:
     def __init__(self, callname, module):
         self.name = callname
@@ -34,10 +31,48 @@ class command:
     def execute(self, additional=""):
         getattr(sys.modules[self.parent_module], self.function)(additional)
 
-def main():
-    return
+# Parses incoming messages and finds/selects the desired command, if exists.
+def read(text):
+    if text.startswith(config.settings['trigger']):
+        message = text[1:].split(" ")
+        valid = 0
+        for module in config.imports:
+            if message[0] in sys.modules[module].includes.keys():
+                valid = 1
+                i = 1
+                pack = sys.modules[module].includes[message[0]]
+                for word in message[1:]:
+                    if word in pack.parameters.keys():
+                        pack = pack.parameters[word]
+                        i += 1
+                if ' '.join(message[1:]) == "help":
+                    print(pack.help())
+                else:
+                    pack.execute(message[1:])
+        if valid == 0:
+            print("Invalid command!")
+
+# Prints all currently imported and supported bot commands.
+commands = command('commands', __name__)
+
+def init():
+    includes.update({commands.name : commands})
+    commands.description = "Lists all currently supported commands, across all active modules using the botParts commands module."
+    commands.function = 'commandsF'
+
+def commandsF(message):
+    currentCommands = "Currently available commands: "
+    for i, module in enumerate(config.imports):
+        for command in sys.modules[module].includes:
+            if i:
+                currentCommands += ", "
+
+            currentCommands += command
+
+    print(currentCommands)
 
 if __name__ == "__main__":
-    main()
+    print("A framework for easily implementing a branching command UI for a chat bot. No main.")
 else:
+    config.imports.append('commandsmodule')
     init()
