b296191d9b395b3925a855631a6949fd7cc1e7d1
dansatk <dansatk@outlook.com>
2021-03-29 20:52:46 -0400

    Added the ability to create timestamped backups of database files.

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ imports = []
 if __name__ == "__main__":
     print("No main.")
 
+backupPath = pathlib.Path.cwd() / 'backup'
 dataPath = pathlib.Path.cwd() / 'data'
 #database = "/home/dansatk/dev/projects/discord/botParts/data/database.db"
