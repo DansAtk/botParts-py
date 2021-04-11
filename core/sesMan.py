bb41a1fa2292f662146fcf17f5563e7b598a8606
dansatk <dansatk@outlook.com>
2021-04-11 19:00:18 -0400

    Finished disrun and sesMan, an alternate run module and a session managing module that provide a login prompt. Logging in as a user and specifying a server facilitates testing dynamic settings and permissions across multiple users and servers.
    It also steers development towards using fewer global settings and using custom values on a per-command basis, based on the user and server context.
    
    Added dynamic trigger selection from the database, with a fallback to the global default trigger stored in config.

--- core/sesMan.py
+++ core/sesMan.py
@@ -16,7 +16,7 @@ def getTrigger():
     global currentServer
 
     if currentServer:
-        trigger = currentserver.trigger
+        trigger = currentServer.trigger
 
     else:
         trigger = None
@@ -59,15 +59,60 @@ def setAlias():
     else:
         return None
 
+def login():
+    userText = input('User: ')
+
+    setUser(userText)
+
+    if currentUser:
+        serverText = input('Server: ')
+
+        setServer(serverText)
+
+        if currentServer:
+            setAlias()
+
+            if currentAlias:
+                print('\nLogged in as {} on server {}.\n'.format(currentUser.name, currentServer.name))
+
+            else:
+                print('\nError: User {} has no alias on server {}.\n'.format(currentUser.name, currentServer.name))
+
+        else:
+            print('\nError: Server not found!\n')
+
+    else:
+        print('\nError: User not found!\n')
+
 def registerCommands():
     global sessionC
     sessionC = command('session', mSelf)
-    sessionC.description = 'Commands for managing the current simulated login session.'
+    sessionC.description = 'Commands for managing the current login session.'
     sessionC.instruction = 'Specify a parameter.'
     global sessionLogoutC
     sessionLogoutC = command('logout', sessionC)
     sessionLogoutC.description = 'Logs you out of the current session.'
     sessionLogoutC.function = 'sessionLogoutF'
+    global userC
+    userC = command('user', mSelf)
+    userC.description = 'Used to alter user settings.'
+    userC.instruction = 'Specify a parameter. By itself displays the current settings.'
+    userC.function = 'userF'
+    global userNameC
+    userNameC = command('name', userC)
+    userNameC.description = 'Used to change the current user\'s name.'
+    userNameC.instruction = 'Specify a new name.'
+    userNameC.function = 'userNameF'
+    global serverC
+    serverC = command('server', mSelf)
+    serverC.description = 'Used to alter server settings.'
+    serverC.instruction = 'Specify a parameter. By itself displays the current settings.'
+    serverC.function = 'serverF'
+    global serverTriggerC
+    serverTriggerC = command('trigger', serverC)
+    serverTriggerC.description = 'Used to alter the current server\'s trigger.'
+    serverTriggerC.instruction = 'Specify a new trigger.'
+    serverTriggerC.function = 'serverTriggerF'
     
 def sessionLogoutF():
     global currentUser
@@ -77,6 +122,49 @@ def sessionLogoutF():
     currentUser = None
     currentServer = None
     currentAlias = None
+
+    print('\nLogged out!\n\n\n')
+
+def serverF():
+    global currentServer
+
+    if currentServer:
+        print('Server {}:'.format(currentServer.id))
+        print('Name: {}'.format(currentServer.name))
+        print('Timezone: {}'.format(currentServer.tz))
+        print('Trigger: {}'.format(currentServer.trigger))
+
+def serverTriggerF(userinput):
+    global currentServer
+    setTrigger = False
+
+    if len(userinput) == 1:
+        if userinput[0].lower() == 'none':
+            if currentServer.trigger:
+                currentServer.trigger = None
+                setTrigger = True
+                print('Server trigger has been removed.')
+
+            else:
+                print('Server trigger is already set to \'None\'')
+
+        else:
+            if len(userinput[0]) < 3:
+                if len(userinput[0]) > 0:
+                    currentServer.trigger = userinput[0]
+                    setTrigger = True
+                    print('Server trigger has been set to {}'.format(userinput[0]))
+
+                else:
+                    print('Please specify at least one character for a trigger.')
+
+            else:
+                print('Try a shorter trigger.')
+    else:
+        print('Please limit the trigger to a single character or small group of characters with no whitespace.')
+
+    if setTrigger:
+        DBM.updateServer(currentServer)
 
 if __name__ == "__main__":
     print('An offline, multiuser and (simulated) multiserver session manager. No main.')
