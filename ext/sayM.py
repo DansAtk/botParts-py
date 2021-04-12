3d9883bbb157167fd0fc44540374837e80a15ab4
DansAtk <dansatk@outlook.com>
2021-04-11 21:27:29 -0400

    Updating Say module commands with instructions.

--- ext/sayM.py
+++ ext/sayM.py
@@ -1,34 +1,44 @@
 import sys
+import random
 
 from core import config
 from core.commandM import command
+from core import DBM
 
 mSelf = sys.modules[__name__]
 includes = {}
 config.imports.append(__name__)
 
-def init():
+def registerCommands():
+    global sayC
     sayC = command('say', mSelf)
     sayC.description = 'Repeats the input text.'
+    sayC.instruction = 'Specify a message to be said, surrounded by quotes (""). Add parameters to change how the message is presented. Enter a term without quotes to pull a related saved quote from the database.'
     sayC.function = 'sayF'
-
-    quietlyC = command('quietly', sayC)
-    quietlyC.description = 'Quietly says things in all lowercase.'
-    quietlyC.function = 'quietlyF'
-    loudlyC = command('loudly', sayC)
-    loudlyC.description = 'Shouts things in all caps.'
-    loudlyC.function = 'loudlyF'
+    global sayQuietlyC
+    sayQuietlyC = command('quietly', sayC)
+    sayQuietlyC.description = 'Quietly says things in all lowercase.'
+    sayQuietlyC.instruction = 'Specify a message to be said quietly, \
+            surrounded by quotes (""). Add parameters to change how the \
+            message is presented. Enter a term without quotes to pull a \
+            related saved quote from the database.'
+    sayQuietlyC.function = 'sayQuietlyF'
+    global sayLoudlyC
+    sayLoudlyC = command('loudly', sayC)
+    sayLoudlyC.description = 'Shouts things in all caps.'
+    sayLoudlyC.instruction = 'Specify a message to be said, surrounded by quotes (""). Add parameters to change how the message is presented. Enter a term without quotes to pull a related saved quote from the database.'
+    sayLoudlyC.function = 'sayLoudlyF'
 
 def sayF(userinput):
     print(' '.join(userinput))
 
-def quietlyF(userinput):
+def sayQuietlyF(userinput):
     print((' '.join(userinput)).lower())
 
-def loudlyF(userinput):
+def sayLoudlyF(userinput):
     print((' '.join(userinput)).upper())
 
 if __name__ == "__main__":
     print('A module for repeating the user\'s input message in various formats. No main.')
 else:
-    init()
+    registerCommands()
