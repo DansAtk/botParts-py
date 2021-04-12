b8bab7fb39e69bd1c6d2f3a25533a3a822082b04
dansatk <dansatk@outlook.com>
2021-04-12 13:00:44 -0400

    Added 'say robot' command to say module.

--- ext/sayM.py
+++ ext/sayM.py
@@ -13,21 +13,32 @@ def registerCommands():
     global sayC
     sayC = command('say', mSelf)
     sayC.description = 'Repeats the input text.'
-    sayC.instruction = 'Specify a message to be said, surrounded by quotes (""). Add parameters to change how the message is presented. Enter a term without quotes to pull a related saved quote from the database.'
+    sayC.instruction = 'Specify a message surrounded by quotes (""). ' \
+            'Add parameters to change how the message is presented. ' \
+            'Enter a term without quotes to pull a related saved quote ' \
+            'from the database.'
     sayC.function = 'sayF'
     global sayQuietlyC
     sayQuietlyC = command('quietly', sayC)
-    sayQuietlyC.description = 'Quietly says things in all lowercase.'
-    sayQuietlyC.instruction = 'Specify a message to be said quietly, \
-            surrounded by quotes (""). Add parameters to change how the \
-            message is presented. Enter a term without quotes to pull a \
-            related saved quote from the database.'
+    sayQuietlyC.description = 'Quietly repeats a message in all lowercase.'
+    sayQuietlyC.instruction = 'Specify a message surrounded by quotes (""). ' \
+            'Enter a term without quotes to pull a related saved quote ' \
+            'from the database.'
     sayQuietlyC.function = 'sayQuietlyF'
     global sayLoudlyC
     sayLoudlyC = command('loudly', sayC)
-    sayLoudlyC.description = 'Shouts things in all caps.'
-    sayLoudlyC.instruction = 'Specify a message to be said, surrounded by quotes (""). Add parameters to change how the message is presented. Enter a term without quotes to pull a related saved quote from the database.'
+    sayLoudlyC.description = 'Shouts a message in all caps.'
+    sayLoudlyC.instruction = 'Specify a message surrounded by quotes (""). ' \
+            'Enter a term without quotes to pull a related saved quote ' \
+            'from the database.'
     sayLoudlyC.function = 'sayLoudlyF'
+    global sayRobotC
+    sayRobotC = command('robot', sayC)
+    sayRobotC.description = 'Translates a message to binary.'
+    sayRobotC.instruction = 'Specify a message surrounded by quotes (""). ' \
+            'Enter a term without quotes to pull a related saved quote ' \
+            'from the database.'
+    sayRobotC.function = 'sayRobotF'
 
 def sayF(userinput):
     print(' '.join(userinput))
@@ -37,6 +48,11 @@ def sayQuietlyF(userinput):
 
 def sayLoudlyF(userinput):
     print((' '.join(userinput)).upper())
+
+def sayRobotF(userinput):
+    inputText = ' '.join(userinput)
+    roboText = (''.join(format(ord(x), '08b') for x in inputText))
+    print(f'\n{roboText}\n')
 
 if __name__ == "__main__":
     print('A module for repeating the user\'s input message in various formats. No main.')
