6699d204cd29d123481a9d90c5cf0ab69e51e137
dansatk <dansatk@outlook.com>
2021-04-25 19:42:23 -0400

    Starting multiple structural changes in module organization. 	- A very basic parent will act as main, importing all other modules. Current config.py will likely take on this role in addition to holding global values. 	- Interface will be in a separate package from core, allowing multiple interfaces to be developed and switched between. 	- Main module will be configured with which interface to load, and which global settings to use. 	- Modules not depending on any specific interface will now be in the core/ext folder. Modules that depend on a specific interface will be in int/(interface name)/ext.
    Separated utility functions from core/DBM into core/util, for functions and commands to manage the database, time and timezones, and generic parent commands like 'add' that can be extended by external modules. (i.e. 'add user').

---
+++ core/util.py
@@ -0,0 +1,266 @@
+import sys
+import os
+import pathlib
+import shutil
+
+import sqlite3
+import pytz
+import calendar
+from datetime import *
+
+from core import config
+from core.commandM import command, request_queue, imports
+
+mSelf = sys.modules[__name__]
+includes = {}
+DB = config.database
+
+def getTime(refTZ=None):
+    if refTZ:
+        return datetime.now(pytz.timezone(refTZ))
+    else:
+        return datetime.now(pytz.timezone('US/Eastern'))
+
+def getTimezone(tzName):
+    return tryGetOneTimezone(tzName)
+
+def tryGetOneTimezone(timezonestring):
+    thisTimezone = None
+
+    searchResults = searchTimezonebyName(timezonestring)
+
+    if searchResults:
+        if len(searchResults) == 1:
+            thisTimezone = searchResults[0]
+
+    return thisTimezone
+
+def searchTimezonebyName(userinput):
+    foundTZ = []
+    
+    for timezone in pytz.all_timezones:
+        if userinput.lower() in timezone.lower():
+            foundTZ.append(timezone)
+
+    if len(foundTZ) > 0:
+        return foundTZ
+    
+    else:
+        return None
+
+def checkDB():
+    if DB.exists():
+        return True
+
+    else:
+        config.debugQ.put('Database not found.')
+        return False
+
+def backupDB():
+    if checkDB():
+        timestamp = (datetime.now()).strftime("%Y%m%d%H%M%S%f")
+    
+        if not config.backupPath.exists():
+            config.backupPath.mkdir()
+
+        backupFile = config.backupPath / ('{dbname}_backup_{code}.db'.format(dbname=config.database.stem, code=timestamp))
+    
+        shutil.copy2(config.database, backupFile)
+
+        return backupFile
+
+def cleanup():
+    if checkDB():
+        oFile = backupDB()
+        config.debugQ.put(f'Database backed up to \'{oFile.name}\'.')
+
+def registerCommands():
+    global databaseC
+    databaseC = command('database', mSelf)
+    databaseC.description = 'Commands for managing the bot\'s database.'
+    databaseC.instruction = 'Specify a parameter. Alone, displays information about the database\'s current state.'
+    databaseC.function = 'databaseF'
+    global databaseSetupC
+    databaseSetupC = command('setup', databaseC)
+    databaseSetupC.description = 'Initializes the database. If the database already exists, it can be reinitialized.'
+    databaseSetupC.function = 'databaseSetupF'
+    global databaseDeleteC
+    databaseDeleteC = command('delete', databaseC)
+    databaseDeleteC.description = 'Deletes the database.' 
+    databaseDeleteC.function = 'databaseDeleteF'
+    global databaseBackupC
+    databaseBackupC = command('backup', databaseC)
+    databaseBackupC.description = 'Creates a backup of the database.'
+    databaseBackupC.function = 'databaseBackupF'
+    global addC
+    addC = command('add', mSelf)
+    addC.description = 'Used to create new objects in the database.'
+    addC.instruction = 'Specify a parameter.'
+    global removeC
+    removeC = command('remove', mSelf)
+    removeC.description = 'Used to remove existing objects from the database.'
+    removeC.instruction = 'Specify a parameter.'
+    global editC
+    editC = command('edit', mSelf)
+    editC.description = 'Updates existing objects with new attributes.'
+    editC.instruction = 'Specify a parameter.'
+    global showC
+    showC = command('show', mSelf)
+    showC.description = 'Displays detailed information about database objects.'
+    showC.instruction = 'Specify a parameter.'
+    global listC
+    listC = command('list', mSelf)
+    listC.description = 'Lists all objects of the given type currently in the database.'
+    listC.instruction = 'Specify a parameter.'
+    global listTimezoneC
+    listTimezoneC = command('timezone', listC)
+    listTimezoneC.description = 'Lists all available timezones.'
+    listTimezoneC.function = 'listTimezoneF'
+    global timeC
+    timeC = command('time', mSelf)
+    timeC.description = 'Displays the current time.'
+    timeC.function = 'timeF'
+    global timeZonesC
+    timeZonesC = command('zones', timeC)
+    timeZonesC.description = 'Lists all available timezones.'
+    timeZonesC.function = 'listTimezoneF'
+    global findC
+    findC = command('find', mSelf)
+    findC.description = 'Searches for objects meeting the given criteria.'
+    findC.instruction = 'Specify a parameter.'
+    global findTimezoneC
+    findTimezoneC = command('timezone', findC)
+    findTimezoneC.description = 'Searches for timezones with names matching the given query.'
+    findTimezoneC.instruction = 'Specify the name of a timezone.'
+    findTimezoneC.function = 'findTimezoneF'
+    imports.update({__name__ : includes})
+
+def databaseF(inputData):
+    config.outQ.put(databaseC.help())
+
+def databaseSetupF(inputData):
+    if not config.dataPath.exists():
+        config.dataPath.mkdir()
+
+    try:
+        doInit = False
+        if DB.exists():
+            thisThread = request_queue(inputData, filter_channel=True, filter_user=True)
+            thisQ = thisThread['queue']
+            config.outQ.put(f'{thisThread["tag"]}> Database already exists. Re-initialize? This will empty the database. ({thisThread["tag"]}> y/N)')
+            rawResponse = thisQ.get()
+            response = ' '.join(rawResponse.content)
+            if response.lower() == 'y':
+                DB.unlink()
+                doInit = True
+            else:
+                config.outQ.put('Canceled.')
+        else:
+            doInit = True
+        
+        if doInit:
+            conn = sqlite3.connect(DB)
+            cursor = conn.cursor()
+            cursor.execute(
+                    "CREATE TABLE info("
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT, key TEXT, value TEXT)"
+                    )
+            cursor.execute("INSERT INTO info(key, value) VALUES (?, ?)",
+                    ('dbversion', config.settings['dbversion']))
+            config.debugQ.put('Configuring for multiple users...')
+            cursor.execute(
+                    "CREATE TABLE users("
+                    "id INTEGER PRIMARY KEY, name TEXT, botrank INTEGER DEFAULT '0', country TEXT, "
+                    "tz TEXT DEFAULT 'US/Eastern', bday TEXT, points INTEGER DEFAULT '0')"
+                    )
+            config.debugQ.put('Configuring for multiple servers...')
+            cursor.execute(
+                    "CREATE TABLE servers("
+                    "id INTEGER PRIMARY KEY, name TEXT, tz TEXT, trigger TEXT)"
+                    )
+            config.debugQ.put('Configuring for color management...')
+            cursor.execute(
+                    "CREATE TABLE colors("
+                    "id INTEGER PRIMARY KEY, name TEXT, code TEXT)"
+                    )
+            config.debugQ.put('Configuring for user aliases...')
+            cursor.execute(
+                    "CREATE TABLE serverusers("
+                    "userid INTEGER NOT NULL, serverid INTEGER NOT NULL, color INTEGER, nick TEXT, localrank TEXT, "
+                    "PRIMARY KEY(userid, serverid), "
+                    "FOREIGN KEY(userid) REFERENCES users(id) ON DELETE CASCADE ON UPDATE NO ACTION, "
+                    "FOREIGN KEY(serverid) REFERENCES servers(id) ON DELETE CASCADE ON UPDATE NO ACTION, "
+                    "FOREIGN KEY(color) REFERENCES colors(id) ON DELETE SET NULL ON UPDATE NO ACTION)" 
+                    )
+            conn.commit()
+            conn.close()
+
+            for module in imports:
+                if hasattr(sys.modules[module], 'dbinit'):
+                    sys.modules[module].dbinit(DB)
+
+            config.debugQ.put('Database initialized.')
+    
+    except:
+        config.debugQ.put('An error occurred.')
+
+def databaseDeleteF(inputData):
+    thisThread = request_queue(inputData, filter_channel=True, filter_user=True)
+    thisQ = thisThread['queue']
+    config.outQ.put(f'{thisThread["tag"]}> Are you sure you want to delete the current database? ({thisThread["tag"]}> y/N)')
+    rawResponse = thisQ.get()
+    response = ' '.join(rawResponse.content)
+
+    if response.lower() == 'y':
+        config.database.unlink()
+        config.outQ.put('Database deleted.')
+
+    else:
+        config.outQ.put('Cancelled.')
+
+def databaseBackupF(inputData):
+    if checkDB():
+        oFile = backupDB()
+        config.debugQ.put(f'Database backed up to \'{oFile.name}\'.')
+
+def listTimezoneF(inputData):
+    output_text = ''
+
+    for i, each in enumerate(pytz.all_timezones):
+        if i:
+            output_text += '\n'
+
+        output_text += each
+
+    config.outQ.put(output_text)
+
+def timeF(inputData):
+    config.outQ.put(f'The current time is {getTime()}.')
+
+def findTimezoneF(inputData, content):
+    timezoneString = ' '.join(content)
+    results = searchTimezonebyName(timezoneString)
+    
+    if results:
+        output_text = ''
+        if len(results) == 1:
+            output_text += 'One timezone found:\n'
+
+        else:
+            output_text += f'{len(results)} timezones found:\n'
+
+        for i, each in enumerate(results):
+            if i:
+                output_text += '\n'
+    
+            output_text += each
+
+        config.outQ.put(output_text)
+
+    else:
+        config.outQ.put('No timezones found!')
+
+if __name__ == "__main__":
+    print("No main.")
+else:
+    registerCommands()
